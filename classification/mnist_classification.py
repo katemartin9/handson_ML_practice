005d509476eff86766cea288f8adbf3bcc821b64
Kate Martin <katmartin.uk@gmail.com>
2020-02-15 18:06:18 +0000

    update

--- classification/mnist_classification.py
+++ classification/mnist_classification.py
@@ -1,12 +1,11 @@
 from sklearn.datasets import fetch_openml
 import pdb
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import StratifiedKFold, cross_val_score
-from sklearn.base import clone
-
+from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_val_predict
+from sklearn.base import clone, BaseEstimator
+from sklearn.metrics import confusion_matrix
 
 mnist = fetch_openml('mnist_784', version=1)
 
@@ -27,25 +26,43 @@ y_train_5 = (y_train == 5)
 y_test_5 = (y_test == 5)
 
 sgd_clf = SGDClassifier(random_state=42)
-#pdb.set_trace()
-print(X_test[0])
+print(y_test[0])
 print(sgd_clf.fit(X_train, y_train_5).predict(X_test[0].reshape(1, -1)))
 
 
 # MEASURE ACCURACY WITH CROSS-VAL
 
 skfolds = StratifiedKFold(n_splits=3, random_state=42)
-
+#pdb.set_trace()
 for train_index, test_index in skfolds.split(X_train, y_train_5):
     clone_clf = clone(sgd_clf)
     X_train_folds = X_train[train_index]
     y_train_folds = y_train_5[train_index]
-    X_test_folds = X_train[test_index]
-    y_test_folds = y_test_5[test_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
     clone_clf.fit(X_train_folds, y_train_folds)
-    y_pred = clone_clf.predict(X_test_folds)
-    n_correct = sum(y_pred == y_test_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
     print(n_correct / len(y_pred))
 
-cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring='accuracy')
+print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring='accuracy'))
+
+
+# BASE CLASSIFIER
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+never5clf = Never5Classifier()
+print(cross_val_score(never5clf, X_train, y_train_5, cv=3, scoring='accuracy'))
+
+
+# CONFUSION MATRIX
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train, cv=3)
+confusion_matrix(y_train_5, y_train_pred)
+
+
 
