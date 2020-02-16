4a9b1367cc24ab3a5cfe11b08fa69a6eaa230a73
Kate Martin <katmartin.uk@gmail.com>
2020-02-16 14:29:52 +0000

    pol reg complete

--- linear_regression/polynomial_regression.py
+++ linear_regression/polynomial_regression.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 import pdb
 
 np.random.seed(42)
@@ -27,3 +30,31 @@ plt.xlabel("X1")
 plt.ylabel("Y")
 plt.show()
 
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)
+    plt.xlabel("Training set size", fontsize=14)
+    plt.ylabel("RMSE", fontsize=14)
+    plt.show()
+
+
+plot_learning_curves(regressor, X, y)
+
+
+"""Pipeline Learning Curve on 10th degree polynomial"""
+polynomial_reg = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=2, include_bias=False)),
+    ("lin_reg", LinearRegression()),
+])
+
+plot_learning_curves(polynomial_reg, X, y)
