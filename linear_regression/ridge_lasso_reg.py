8d1638026ac2edab3cbc122ce184b82d18bd1c46
Kate Martin <katmartin.uk@gmail.com>
2020-02-19 20:25:45 +0000

    finished linear reg

--- linear_regression/ridge_lasso_reg.py
+++ linear_regression/ridge_lasso_reg.py
@@ -1,6 +1,10 @@
-from sklearn.linear_model import Ridge, Lasso, SGDRegressor
+from sklearn.linear_model import Ridge, Lasso, SGDRegressor, ElasticNet, LinearRegression
 import numpy as np
 import pdb
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 np.random.seed(42)
 m = 20
@@ -31,8 +35,42 @@ sgd_reg = SGDRegressor(penalty='l1')
 sgd_reg.fit(X, y.ravel())
 print(sgd_reg.predict([[1.5]]))
 
+
 """Elastic Net"""
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
 
 
 """Early Stopping"""
 
+from sklearn.base import clone
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 2 + X + 0.5 * X**2 + np.random.randn(m, 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(), test_size=0.5, random_state=10)
+
+poly_scaler = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+    ("std_scaler", StandardScaler()),
+])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True, penalty=None,
+                       learning_rate='constant', eta0=0.0005)
+
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_val_predict = sgd_reg.fit(X_train_poly_scaled)
+    val_error = mean_squared_error(y_val, y_val_predict)
+    if val_error < minimum_val_error:
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
