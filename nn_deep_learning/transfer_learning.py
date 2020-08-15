dbe8321bd4f37749d8eb72b7fe3e73ca2c1cf1e8
Kate Martin <katmartin.uk@gmail.com>
2020-08-15 21:54:37 +0100

    finished transfer learning part

--- nn_deep_learning/transfer_learning.py
+++ nn_deep_learning/transfer_learning.py
@@ -1,6 +1,9 @@
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+from functools import partial
 
 (X_train, y_train), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
 X_valid, X_train = X_train[:5000] / 255.0, X_train[5000:] / 255.0
@@ -37,6 +40,7 @@ history_a = model_A.fit(X_train_A, y_train_A,
                       epochs=20,
                       validation_data=(X_valid_A, y_valid_A))
 model_A.save('my_model_A.h5')
+
 # MODEL B
 model_B = keras.models.Sequential()
 model_B.add(keras.layers.Flatten(input_shape=[28, 28]))
@@ -71,3 +75,148 @@ for layer in model_B_on_A.layers[:-1]:
 history = model_B_on_A.fit(X_train_B, y_train_B,
                            epochs=16,
                            validation_data=(X_valid_B, y_valid_B))
+
+# CNN
+dataset, info = tfds.load("tf_flowers", as_supervised=True, with_info=True)
+dataset_size = info.splits["train"].num_examples
+class_names = info.features["label"].names
+n_classes = info.features['label'].num_classes
+
+test_set_raw, valid_set_raw, train_set_raw = tfds.load(
+    'tf_flowers',
+    split=['train[:10%]', 'train[10%:25%]', 'train[25%:]'],
+    as_supervised=True)
+
+plt.figure(figsize=(12, 10))
+index = 0
+for image, label in train_set_raw.take(9):
+    index += 1
+    plt.subplot(3, 3, index)
+    plt.imshow(image)
+    plt.title(f"Class: {class_names[label]}")
+    plt.axis('off')
+plt.show()
+
+
+# Basic preprocessing
+def preprocess(image, label):
+    resized_image = tf.image.resize(image, [224, 224])
+    final_image = keras.applications.xception.preprocess_input(resized_image)
+    return final_image, label
+
+
+# More advanced preprocessing / data augmentation
+def central_crop(image):
+    shape = tf.shape(image)
+    min_dim = tf.reduce_min([shape[0], shape[1]])
+    top_crop = (shape[0] - min_dim) // 4
+    bottom_crop = shape[0] - top_crop
+    left_crop = (shape[1] - min_dim) // 4
+    right_crop = shape[1] - left_crop
+    return image[top_crop:bottom_crop, left_crop::right_crop]
+
+
+def random_crop(image):
+    """Randomly crops images"""
+    shape = tf.shape(image)
+    min_dim = tf.reduce_min([shape[0], shape[1]]) * 90 // 100
+    return tf.image.random_crop(image, [min_dim, min_dim, 3])
+
+
+def preprocess_complex(image, label, randomize=False):
+    """Randomly flips images horizontally"""
+    if randomize:
+        cropped_image = random_crop(image)
+        cropped_image = tf.image.random_flip_left_right(cropped_image)
+    else:
+        cropped_image = central_crop(image)
+    resized_image = tf.image.resize(cropped_image, [224, 224])
+    final_image = keras.applications.xception.preprocess_input(resized_image)
+    return final_image, label
+
+
+batch_size = 32
+train_set = train_set_raw.shuffle(1000).repeat()
+train_set = train_set.map(partial(preprocess_complex, randomize=True))\
+                .batch(batch_size)\
+                .prefetch(1)
+valid_set = valid_set_raw.map(preprocess).batch(batch_size).prefetch(1)
+test_set = test_set_raw.map(preprocess).batch(batch_size).prefetch(1)
+
+plt.figure(figsize=(12, 12))
+for X_batch, y_batch in train_set.take(1):
+    for index in range(9):
+        plt.subplot(3, 3, index + 1)
+        plt.imshow(X_batch[index] / 2 + 0.5)
+        plt.title("Class: {}".format(class_names[y_batch[index]]))
+        plt.axis('off')
+plt.show()
+
+
+plt.figure(figsize=(12, 12))
+for X_batch, y_batch in test_set.take(1):
+    for index in range(9):
+        plt.subplot(3, 3, index + 1)
+        plt.imshow(X_batch[index] / 2 + 0.5)
+        plt.title("Class: {}".format(class_names[y_batch[index]]))
+        plt.axis("off")
+
+plt.show()
+
+# load Xception model excluding top layers
+base_model = keras.applications.xception.Xception(weights='imagenet',
+                                                  include_top=False)
+# setting our own average pooling layer and a dense layer
+avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = keras.layers.Dense(n_classes, activation='softmax')(avg)
+model = keras.models.Model(inputs=base_model.input, outputs=output)
+
+for index, layer in enumerate(base_model.layers):
+    print(index, layer.name)
+
+# freezing the weights of the pretrained layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+optimizer = keras.optimizers.SGD(lr=0.2, momentum=0.9, decay=0.01)
+model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer,
+              metrics=['accuracy'])
+history = model.fit(train_set,
+                    steps_per_epoch=int(0.75 * dataset_size / batch_size),
+                    validation_data=valid_set,
+                    validation_steps=int(0.15 * dataset_size / batch_size),
+                    epochs=5)
+# once val accuracy reaches around 75-80% unfreeze layers
+for layer in base_model.layers:
+    layer.trainable = True
+
+optimizer = keras.optimizers.SGD(lr=0.01, momentum=0.9,
+                                 nesterov=True, decay=0.001)
+model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer,
+              metrics=['accuracy'])
+history = model.fit(train_set,
+                    steps_per_epoch=int(0.75 * dataset_size / batch_size),
+                    validation_data=valid_set,
+                    validation_steps=int(0.15 * dataset_size / batch_size),
+                    epochs=40)
+
+# CLASSIFICATION AND LOCALISATION
+base_model2 = keras.applications.xception.Xception(weights='imagenet',
+                                                  include_top=False)
+avg2 = keras.layers.GlobalAveragePooling2D()(base_model.output)
+class_output = keras.layers.Dense(n_classes, activation='softmax')(avg)
+loc_output = keras.layers.Dense(4)(avg)  # bounding box - height, width, horiz and vert coord
+model = keras.models.Model(inputs=base_model.input, outputs=[class_output, loc_output])
+model.compile(loss=["sparse_categorical_crossentropy", "mse"],
+              loss_weights=[0.8, 0.2],
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+
+def add_random_bounding_boxes(images, labels):
+    fake_bboxes = tf.random.uniform([tf.shape(images)[0], 4])
+    return images, (labels, fake_bboxes)
+
+
+fake_train_set = train_set.take(5).repeat(2).map(add_random_bounding_boxes)
+model.fit(fake_train_set, steps_per_epoch=5, epochs=2)
